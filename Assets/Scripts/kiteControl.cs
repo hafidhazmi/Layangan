60d4175fc121308f5d53a390f8e85e9cb1429d3e
hafidhazmi <hafidhazmi026@gmail.com>
2015-04-10 01:12:59 +0700

    Layangan1
    kenapa layangan ini dibakar?

---
+++ Assets/Scripts/kiteControl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class kiteControl : MonoBehaviour {
+
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W)) {
+			this.rotasi ();
+		} else if (Input.GetKey (KeyCode.E)) {
+			this.gas ();
+		} else {
+			this.turun ();
+		}
+	}
+
+	public void gas() {
+		transform.Translate(0, 0.2f, 0);
+	}
+
+	public void rotasi() {
+		transform.Rotate(0, 0, 10);
+	}
+
+	public void turun() {
+		transform.Translate(0, -0.1f, 0);
+	}
+}
---
+++ Assets/Scripts/throttle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class throttle : MonoBehaviour {
+
+	Animator anim;
+
+	public kiteControl kite;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.A)) {
+			Debug.Log("ini sentuhan");
+			kite.gas();
+		}
+	}
+}
